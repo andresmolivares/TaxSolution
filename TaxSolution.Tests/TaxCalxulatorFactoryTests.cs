6412128143ce3b57477111311ce57700f24aae8d
Andres <andresmolivares@hotmail.com>
2021-10-18 21:01:17 -0400

    All projects Enabled nullable type references Refactored null handling accordingly
    API
    Added new TaxJarConfiguration to read token data from config
    Injected TaxJarConfiguration into calculator factory
    Added token to appsetting.json
    
    Desktop
    Organized supporting class files into new Model folder
    
    Models
    Organized tax class files into new respective folders
    Added IGetServiceConnection
    
    Server
    Added TaxJarConfiguration to the factory workflow
    Added TaxJarBaseClientCalculator to provide base plumbing for HttpClient and WebClient based calculators
    
    Tests
    Updated base test class with GetTaxConfig support

---
+++ TaxSolution.Tests/TaxCalxulatorFactoryTests.cs
@@ -0,0 +1,31 @@
+﻿using NUnit.Framework;
+using System;
+using System.Threading.Tasks;
+using TaxSolution.Models;
+using TaxSolution.Server;
+
+namespace TaxSolution.Tests
+{
+    public class TaxCalxulatorFactoryTests : BaseTaxJarTesting
+    {
+        [TestCaseSource(nameof(LoadCalcKeyTestCases))]
+        [Test]
+        public async Task InstantiateCalculatorTest(string key)
+        {
+            var calculator = TaxCalculatorFactory.GetCalculatorInstance(key, GetTaxConfig());
+            Assert.IsInstanceOf<ITaxCalculator>(calculator);
+            await Task.Yield();
+        }
+
+        [Test]
+        public async Task InstantiateInvalidCalculatorTest()
+        {
+            Assert.Throws<ArgumentException>(() =>
+            {
+                var key = "invalid";
+                var calculator = TaxCalculatorFactory.GetCalculatorInstance(key, GetTaxConfig());
+            });
+            await Task.Yield();
+        }
+    }
+}
